0066633c6ab94020644298fac1a745b60ac91687
FazLaCool <fa.ouardirhi@gmail.com>
2024-05-16 15:43:10 +0100

    use

--- mysql/app.js
+++ mysql/app.js
@@ -1,12 +1,24 @@
 import express from 'express'
 
 import { villeRouter } from './routes/villeRouter'
+import { transportRouter } from './routes/transportRouter'
+import { touristeRouter } from './routes/touristeRouter'
+import { santeRouter } from './routes/santeRouter'
+import { paysageRouter } from './routes/paysageRouter'
+import { monumentRouter } from './routes/monumentRouter'
+import { etablissementRouter } from './routes/etablissementRouter'
 
 const app=express()
 
 app.use(express.json())
 
-app.use('/villes',villeRouter)
+app.use('/vi',villeRouter)
+app.use('/tro',transportRouter)
+app.use('/tou',touristeRouter)
+app.use('/san',santeRouter)
+app.use('/pay',paysageRouter)
+app.use('/mon',monumentRouter)
+app.use('/eta',etablissementRouter)
 
 app.use((err, req, res, next) => {
     console.error(err.stack)

b1fb29cf6a2fb5e8151a1d20666c8669970d188e
ayairis <ayaalae12.2002@gmail.com>
2024-06-08 01:16:36 +0100

    comm

--- Front-end/guide-touristique/src/Pages/Admin.jsx
+++ Front-end/guide-touristique/src/Pages/Admin.jsx
@@ -1,9 +1,9 @@
-import NavAdmin from "../Components/NavBar/NavAdmin";
+
 import Container12 from "../Components/Containers/container12/container12.jsx";
+import NavAdmin from "../Components/NavBar/NavAdmin";
+
 
 const Admin = () => {
-    const location = useLocation();
-    const nom = location.state.nom;
 
     return (
         <div>
--- Front-end/guide-touristique/src/main.jsx
+++ Front-end/guide-touristique/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Admin from './Pages/Admin'
 import './index.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Admin />
+    <App />
   </React.StrictMode>,
 )

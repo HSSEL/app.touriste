ea6ac3a3eca2da633fcc01218145e6c45fd95638
HibaZouhair <hibazhr200@gmail.com>
2024-06-10 22:36:41 +0100

    commit

--- Front-end/guide-touristique/src/main.jsx
+++ Front-end/guide-touristique/src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import Admin from './Pages/Admin/Admin'
+import React from 'react'; // Importation de la bibliothèque React
+import ReactDOM from 'react-dom/client'; // Importation d'une méthode spécifique de ReactDOM
+import './index.css'; // Importation du fichier CSS principal
+import App from './App'; // Importation du composant racine de l'application
+import Admin from './Pages/Admin/Admin'; // Importation du composant Admin (peut-être pour une utilisation future)
 
+// Utilisation de ReactDOM.createRoot pour initialiser le rendu de l'application
+// Utilisé pour afficher les éléments React sur la page web
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  ///*Utilisation de React.StrictMode pour des avertissements supplémentaires en mode développement*/
+  <React.StrictMode> 
+   
+    <App /> 
+
   </React.StrictMode>,
-)
+);
+//Rendu du composant racine de l'application, probablement l'App.js

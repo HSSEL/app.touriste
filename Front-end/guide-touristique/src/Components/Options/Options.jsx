779cdedfc05582538f728ad5ea6196e38bf78561
ayairis <ayaalae12.2002@gmail.com>
2024-06-10 18:13:43 +0100

    moiii

--- Front-end/guide-touristique/src/Components/Options/Options.jsx
+++ Front-end/guide-touristique/src/Components/Options/Options.jsx
@@ -2,19 +2,25 @@ import './Options.css'
 import icon1 from '../../assets/Options/icon1.svg'
 import icon2 from '../../assets/Options/icon2.svg'
 import icon3 from '../../assets/Options/icon3.svg'
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Options = () => {
+
+  const location = useLocation();
 
   const navigate = useNavigate();
   const handlelogout = () => {
     navigate('/logout')
   }
 
+  const handleprof = () => {
+    navigate('/usertouriste', {state: { ...location.state }})
+  }
+
     return (
     <div className='option'>
             
-        <img src={icon1} alt=''/>
+        <img onClick={handleprof} src={icon1} alt=''/>
         <img src={icon3} alt=''/>
         <img onClick={handlelogout} src={icon2} alt=''/>
 

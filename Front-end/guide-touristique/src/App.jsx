fbda5c7f585fdae5053e16ad9d3d84c5c98553f5
HibaZouhair <hibazhr200@gmail.com>
2024-06-10 22:54:38 +0100

    commit

--- Front-end/guide-touristique/src/App.jsx
+++ Front-end/guide-touristique/src/App.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from 'react'; // Importation des fonctions React nécessaires pour définir les composants fonctionnels et pour gérer les effets de bord.
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Importation des composants nécessaires pour la gestion des routes dans l'application à l'aide de React Router.
+
+// Importations des différents composants de pages utilisés dans l'application.
 import Home from './Pages/Home';
-import Comment from './Pages/Comment'
-import Etabs from './Pages/Etabs'
-import Services from './Pages/Services'
+import Comment from './Pages/Comment';
+import Etabs from './Pages/Etabs';
+import Services from './Pages/Services';
 import Etabviewmore from './Pages/Etabviewmore';
 import Villes from './Pages/Villes';
 import Ville from './Pages/Ville';
@@ -11,7 +14,7 @@ import Reetab from './Pages/Reetab';
 import Reserver from './Pages/Reserver';
 import Logout from './Pages/Logout';
 import New from './Pages/New';
-import TouristesPage from './Pages/Admin/Touristes'
+import TouristesPage from './Pages/Admin/Touristes';
 import Admin from './Pages/Admin/Admin';
 import EtabPage from './Pages/Admin/Etablissement';
 import Villesadmin from './Pages/Admin/Villesadm';
@@ -21,12 +24,13 @@ import UserEtab from './Pages/Users/UserEtab';
 import ContactForm from './Pages/ContactForm';
 import Ajouterpub from './Pages/Users/Ajouterpub';
 
-const App = () => {
-    const [publications, setPublications] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+const App = () => { // Déclaration du composant principal de l'application, nommé `App`, en tant que fonction fléchée.
 
-    useEffect(() => {
+    const [publications, setPublications] = useState([]); // Déclaration de l'état `publications` à l'aide du Hook `useState`, qui stockera les données des publications récupérées depuis l'API.
+    const [loading, setLoading] = useState(true); // Déclaration de l'état `loading` à l'aide du Hook `useState`, qui indiquera si les données sont en cours de chargement.
+    const [error, setError] = useState(null); // Déclaration de l'état `error` à l'aide du Hook `useState`, qui contiendra les erreurs éventuelles lors du chargement des données.
+
+    useEffect(() => { // Utilisation du Hook `useEffect` pour effectuer une requête API lors du chargement initial du composant afin de récupérer les publications.
         const fetchPublications = async () => {
             try {
                 const response = await fetch('http://localhost:8080/pub/Publications');
@@ -42,17 +46,17 @@ const App = () => {
         fetchPublications();
     }, []);
 
-    if (loading) {
+    if (loading) { // Si l'application est en cours de chargement (`loading` est vrai), un message "Loading..." est affiché.
         return <div>Loading...</div>;
     }
 
-    if (error) {
+    if (error) { // Si une erreur survient lors du chargement des données (`error` nul), un message d'erreur est affiché.
         return <div>Error: {error.message}</div>;
     }
 
     return (
         <div>
-            {/*
+            {/* 
             <h1>Publications</h1>
             <ul>
                 {publications.map((publication) => (
@@ -63,35 +67,36 @@ const App = () => {
                 ))}
             </ul>
             */}
-            <BrowserRouter>
-            <Routes>
-            <Route index element={<Home/>} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/comment" element={<Comment/>} />
-            <Route path="/etabs" element={<Etabs/>} />
-            <Route path="/services" element={<Services/>} />
-            <Route path="/etab" element={<Etabviewmore/>} />
-            <Route path="/villes" element={<Villes/>} />
-            <Route path="/ville" element={<Ville/>} />
-            <Route path="/reetab" element={<Reetab/>} />
-            <Route path="/reserver" element={<Reserver/>} />
-            <Route path="/logout" element={<Logout/>} />
-            <Route path="/new" element={<New/>} />
-            <Route path="/touristes" element={<TouristesPage/>} />
-            <Route path="/admin" element={<Admin/>} />
-            <Route path="/etablissements" element={<EtabPage/>} />
-            <Route path="/villesadm" element={<Villesadmin/>} />
-            <Route path="/pubadmin" element={<PubAdmin/>} />
-            <Route path="/usertouriste" element={<UserTouriste/>} />
-            <Route path="/useretab" element={<UserEtab/>} />
-            <Route path="/ContactForm" element={<ContactForm />} />
-            <Route path="/ajouterpub" element={<Ajouterpub />} />
-     
-            </Routes> 
+            
+            <BrowserRouter> 
+                {/* //Mise en place du routage avec React Router à l'aide du composant `BrowserRouter` qui encapsule les routes de l'application */}
+                <Routes>
+                    {/* Définition des routes de l'application à l'aide du composant `Routes` et des composants `Route`. */}
+                    <Route index element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/comment" element={<Comment />} />
+                    <Route path="/etabs" element={<Etabs />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/etab" element={<Etabviewmore />} />
+                    <Route path="/villes" element={<Villes />} />
+                    <Route path="/ville" element={<Ville />} />
+                    <Route path="/reetab" element={<Reetab />} />
+                    <Route path="/reserver" element={<Reserver />} />
+                    <Route path="/logout" element={<Logout />} />
+                    <Route path="/new" element={<New />} />
+                    <Route path="/touristes" element={<TouristesPage />} />
+                    <Route path="/admin" element={<Admin />} />
+                    <Route path="/etablissements" element={<EtabPage />} />
+                    <Route path="/villesadm" element={<Villesadmin />} />
+                    <Route path="/pubadmin" element={<PubAdmin />} />
+                    <Route path="/usertouriste" element={<UserTouriste />} />
+                    <Route path="/useretab" element={<UserEtab />} />
+                    <Route path="/ContactForm" element={<ContactForm />} />
+                    <Route path="/ajouterpub" element={<Ajouterpub />} />
+                </Routes>
             </BrowserRouter>
         </div>
     );
 };
 
-export default App;
-
+export default App; // Exportation du composant `App` pour être utilisé dans d'autres parties de l'application.
--- Front-end/guide-touristique/src/Pages/Villes.jsx
+++ Front-end/guide-touristique/src/Pages/Villes.jsx
@@ -1,20 +1,19 @@
-import BG from "../Components/BG/BG";
-import NavBar from "../Components/NavBar/NavBar";
-import Options from "../Components/Options/Options";
-import React from 'react';
-import Container8 from "../Components/Containers/Container8/Container8";
+import BG from "../Components/BG/BG"; // Importation du composant BG qui gère l'affichage de l'arrière-plan.
+import NavBar from "../Components/NavBar/NavBar"; // Importation du composant NavBar qui représente la barre de navigation.
+import Options from "../Components/Options/Options"; // Importation du composant Options qui affiche les options de navigation.
+import React from 'react'; // Importation de la bibliothèque React.
+import Container8 from "../Components/Containers/Container8/Container8"; // Importation du composant Container8 qui définit un conteneur avec une mise en page spécifique.
 
+const Villes = () => { // Déclaration du composant Villes en tant que fonction fléchée.
 
-const Villes = () => {
-  
-    return (
-        <div>
-            <BG/>
-            <NavBar/>
-            <Container8/> 
-            <Options/>
-        </div>
+    return ( // Rendu du composant.
+        <div> {/* Début de la division principale */}
+            <BG/> {/* Rendu du composant BG pour afficher l'arrière-plan */}
+            <NavBar/> {/* Rendu du composant NavBar pour afficher la barre de navigation */}
+            <Container8/>  {/* Rendu du composant Container8 pour définir un conteneur avec une mise en page spécifique */}
+            <Options/> {/* Rendu du composant Options pour afficher les options de navigation */}
+        </div> /* Fin de la division principale */
     );
 };
 
-export default Villes;
+export default Villes; // Exportation du composant Villes pour être utilisé dans d'autres parties de l'application.

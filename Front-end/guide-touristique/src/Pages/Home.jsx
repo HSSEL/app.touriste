4952af82e43fc90787d475ffed7ccf76736f7ad9
ayairis <ayaalae12.2002@gmail.com>
2024-06-10 15:26:42 +0100

    amaa

--- Front-end/guide-touristique/src/Pages/Home.jsx
+++ Front-end/guide-touristique/src/Pages/Home.jsx
@@ -4,21 +4,31 @@ import Options from "../Components/Options/Options";
 import Container1 from "../Components/Containers/Container1/Container1";
 import Container2 from "../Components/Containers/Container2/Container2";
 import Container3 from "../Components/Containers/Container3/Container3";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 
 const Home = () => {
-  
 
-  return (
-    <div>
-      <BG/>
-      <NavBar/>
-      <Container1/>
-      <Container2/>
-      <Container3/>
-      <Options/>
-    </div>
-  );
+    const location = useLocation();
+    const { state } = location;
+
+    useEffect(() => {
+        if (state) {
+            console.log('Received state:', state);
+        }
+    }, [state]);
+
+    return (
+        <div>
+            <BG/>
+            <NavBar/>
+            <Container1/>
+            <Container2/>
+            <Container3/>
+            <Options/>
+        </div>
+    );
 };
 
 export default Home;
